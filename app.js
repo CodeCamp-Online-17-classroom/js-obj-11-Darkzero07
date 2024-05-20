f6172878c48319177c02bbc3189117648d388c20
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 08:37:23 +0700

    js lab 57

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// write code here
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter product name : ");
+let quantity = parseInt(prompt("Enter quantity  : "));
+let pricePerUnit = parseFloat(prompt("Enter pricePerUnit : "));
+let discount = parseFloat(prompt("Enter discount : "));
+
+function creatProduct(name, quantity, pricePerUnit, discount) {
+  let product = {
+    name: name,
+    quantity: quantity,
+    pricePerUnit: pricePerUnit,
+  };
+  if (discount > 0) {
+    product.discount = discount;
+  }
+  return product;
+}
+
+function totalPrice(product) {
+  let price = product.pricePerUnit * product.quantity;
+  if (product.discount) {
+    price = price * (1 - product.discount / 100);
+  }
+  return price;
+}
+
+let product = creatProduct(name, quantity, pricePerUnit, discount);
+console.log(product);
+
+let amountPrice = totalPrice(product);
+console.log(amountPrice);
